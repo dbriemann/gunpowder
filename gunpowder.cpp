ba6238dbf77e6ff4bad50b5e1bfcb6d1ec8af421
David Linus Briemann <dbriemann@gmail.com>
2013-09-17 18:32:26 +0200

    init repo

---
+++ gunpowder.cpp
@@ -0,0 +1,34 @@
+
+/*
+ * Author: David Briemann
+ *
+ * Entry for the CodeCup 2014 coding contest.
+ * A computer player for the game 'Poly-Y'.
+ *
+ * This is all packed into one file due to tournament/upload rules.
+ *
+ * Description:
+ * Program has to play the game of 'Poly-Y' against other computer players.
+ * Communication is done via stdin/-out and a manager program.
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+/*
+ * global definitions
+ */
+
+/*
+ * Structs
+ */
+
+
+/*
+ * gunpowder main
+ */
+int main() {
+
+    return 0;
+}
