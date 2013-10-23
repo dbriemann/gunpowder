4b09a03d37c735d490396e421391e9fef792ff39
David Linus Briemann <dbriemann@gmail.com>
2013-10-23 15:14:05 +0200

    revision v0.3 plain mc + amaf for own stones + uct selection

--- PlainMC.hpp
+++ PlainMC.hpp
@@ -66,68 +66,64 @@ U8 PlainMCEngine::runSim(double remaining_time) {
 
     U8 win_color = NO_WIN;
     U8 best_move = board.possible_moves[0];
+    U8 next_move = best_move;
+    double best_score = 0.0;
 
     cerr << "   --- Run simulation for move no. " << (int)board.next_move << " ---" << endl;
     cerr << "   --- Turn time: " << turn_time << " sec." << endl;
 
     while(!wtimer.out_of_time(turn_time)) {
         //selection -> TODO: improve
-        //UCTValue(parent, n) = winrate + sqrt((ln(parent.visits))/(5*n.nodevisits))
-//        U8 pm = board.possible_moves[0];
-//        double uct = 0;
-//        double simlog = log(simulations);
-//        for(U8 m : board.possible_moves) {
-//            double u = (double) results[m].wins / (double) results[m].updates
-//                    + sqrt(simlog / (5.0*(results[m].selections+1)));
-//            if(u > uct) {
-//                pm = m;
-//                uct = u;
-//            }
-//        }
-        U8 r = fastrand() % board.possible_moves.size(); //best_move;
-        U8 pm = board.possible_moves[r];
+//        U8 r = fastrand() % board.possible_moves.size(); //best_move;
+//        next_move = board.possible_moves[r];
 
-        results[pm].selections++;
+        results[next_move].selections++;
         sim_board = board;
-        sim_board.makeMove(pm);
+        sim_board.makeMove(next_move);
 
         //mc simulation
         win_color = sim_board.randomFill();
 
-        //TODO
-        //use fastboard.possible_moves as a move_history...
-        //weight importance with distance to current board
+        simulations++;
+
+        double uct = 0;
+        double simlog = log(simulations);
 
         for(U8 m : board.possible_moves) {
             //from player perspective
             if(sim_board.stones[m] == board.to_play) {
-                results[m].update(COLOR_WIN_BONUS[win_color]);
-//                results[m].update(WIN_TRANSLATION[board.to_play][win_color]);
-            } else {
-                results[m].update(-COLOR_WIN_BONUS[win_color]);
+//                results[m].update(COLOR_WIN_BONUS[win_color]);
+                results[m].update(WIN_TRANSLATION[board.to_play][win_color]);
             }
-        }
+//            else {
+//                results[m].update(-COLOR_WIN_BONUS[win_color]);
+//            }
 
-        simulations++;
+            //UCTValue(parent, n) = winrate + sqrt((ln(parent.visits))/(n.nodevisits))
+//            I32 rel_score = results[m].wins * COLOR_WIN_BONUS[board.to_play];
+//            double score = ((double) results[m].updates + (double) rel_score) * 0.5 / (double)results[m].updates;
+            double score = (double)results[m].wins / (double) results[m].updates;
+            double u = score + sqrt(simlog / (results[m].selections+1));
+
+            if(u > uct) {
+                next_move = m;
+                uct = u;
+            }
+        }        
     }
-
 
     cerr << "   --- " << simulations << " simulations run." << endl;
 
     U32 sel = 0;
 
-    I32 best_rel_score = -1000000;
-    double best_score = 0.0;
-
     for(int i = FIRST_FIELD; i <= LAST_FIELD; i++) {
         if(board.stones[i] == EMPTY) {
-            I32 rel_score = results[i].wins * COLOR_WIN_BONUS[board.to_play];
-            double score = ((double) results[i].updates + (double) rel_score) * 0.5 / (double)results[i].updates;
-//            double score = (double)results[i].wins / (double) results[i].updates;
+//            I32 rel_score = results[i].wins * COLOR_WIN_BONUS[board.to_play];
+//            double score = ((double) results[i].updates + (double) rel_score) * 0.5 / (double)results[i].updates;
+            double score = (double)results[i].wins / (double) results[i].updates;
             cerr << " # Move: " << (int) i << " Score: " << score*100.0 << "% Updates: " << results[i].updates << " Selections: " << results[i].selections << endl;
 
-            if(rel_score > best_rel_score) {
-                best_rel_score = rel_score;
+            if(results[i].selections > sel) {
                 best_score = score;
                 sel = results[i].selections;
                 best_move = i;
